b67977c4e5e200b1dd5ead6427026c38f9da69aa
kamalkchoudhary133 <kamalkchoudhary133@gmail.com>
2021-02-21 18:11:32 +0530

    To retrieve links and print them(for now)

--- WebCrawler.py
+++ WebCrawler.py
@@ -10,12 +10,23 @@ from bs4 import BeautifulSoup
 import argparse
 
 # Now lets write code for making CLI tool using argparse module.
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', help="Enter the target URL")
+parser.add_argument("--url", help="Enter the target URL")
 args = parser.parse_args()
-
 # I will try to add more arguments into my CLI tool.
+
+# The following code will help us to get all the links from a given webpage.
+
+# We will first make a HTTP request to the given url.
+source_code = requests.get(args.url)
+plain_text = source_code.text
+# Now we will load it into a Beautiful Soup object.
+soup = BeautifulSoup(plain_text, 'html.parser')
+urls = []
+# Now we will get all links using .findAll() and use "a" tag which the indicator for links in html.
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
 
 
 
