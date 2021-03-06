c9c202b1061bbad40d104d08374bf39e854bdb56
kamalkchoudhary133 <kamalkchoudhary133@gmail.com>
2021-03-06 13:35:26 +0530

    Finds  emails in webpages

---
+++ HarvestMails.py
@@ -0,0 +1,14 @@
+def harvest_email(url):
+    email = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[
+    \x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[
+    a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(
+    5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[
+    \x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) """
+    r = requests.get(url)
+    for re_match in re.finditer(email, r.text):
+        file_2 = open("Mails.txt", 'a')
+        file_2.write(re_match.group())
+        file_2.close()
+
+
+harvest_email(links)
